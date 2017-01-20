dede2aac14256e1352151d0e156e29895f8eb62f
CliffLee <leecliff@usc.edu>
2017-01-19 17:43:39 -0800

    Added logger configuration (DEBUG3) and switched test ports/ids for user input

--- src/share/examples/acomms/modemdriver/driver_evologics/driver_evologics.cpp
+++ src/share/examples/acomms/modemdriver/driver_evologics/driver_evologics.cpp
@@ -9,24 +9,33 @@ using goby::acomms::operator<<;
 
 int main(int argc, char* argv[]) {
 
-	if(argc < 3)
+
+	// Input handling
+	// CL: Probably only need tty for port, name for glog, and modem id for testing
+	// CL: We can add more checks later if necessary
+	if(argc < 2)
 	{
-		std::cout << "usage: driver_simple /dev/tty_of_modem modem_id [type: MMDriver (default)|ABCDriver]" << std::endl;
+		std::cout << "usage: driver_evologics /dev/tty_of_modem modem_id" << std::endl;
 		return 1;
 	}
 
+	// 0.5
+	// Configuring glog
+	goby::glog.set_name(argv[0]);
+	goby::glog.add_stream(goby::common::logger::DEBUG3, &std::clog); // CL: Set DEBUG3, not sure if final
+
 	// 1
 	// Initialize a base driver and cfg
-	// To be configured later
+	// CL: To be configured later
 	goby::acomms::ModemDriverBase* driver = 0;
 	goby::acomms::protobuf::DriverConfig cfg;
 
-	// TODO set up ethernet connection here
-	cfg.set_modem_id(1);									// test purpose id
+	// Configure ethernet connection here
+	cfg.set_modem_id(goby::util::as<uint32>(argv[2]));	// CL: user input id
 	cfg.set_connection_type(goby::acomms::protobuf::DriverConfig::CONNECTION_TCP_AS_CLIENT);
-	cfg.set_tcp_server("127.0.0.1"); 			// test purpose ip address
+	cfg.set_tcp_server("127.0.0.1"); 										// CL: test purpose ip address
 	cfg.set_reconnect_interval(1);
-
+	cfg.set_tcp_port(goby::util::as<uint32>(argv[1])); 	// CL: user input port
 	// Default to Evologics Driver
 	if (!driver)
 	{
