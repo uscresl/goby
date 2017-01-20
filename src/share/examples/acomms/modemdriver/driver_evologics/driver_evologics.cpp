3e861e6779073f1c3aa288eed7ffffdc70243b60
CliffLee <leecliff@usc.edu>
2017-01-20 14:28:58 -0800

    Created CMakeLists for driver evologics

--- src/share/examples/acomms/modemdriver/driver_evologics/driver_evologics.cpp
+++ src/share/examples/acomms/modemdriver/driver_evologics/driver_evologics.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
 
 	// 0.5
 	// Configuring glog
-	goby::glog.set_name(argv[0]);
+	goby::glog.set_name(argv[1]);
 	goby::glog.add_stream(goby::common::logger::DEBUG3, &std::clog); // CL: Set DEBUG3, not sure if final
 
 	// 1
@@ -31,11 +31,11 @@ int main(int argc, char* argv[]) {
 	goby::acomms::protobuf::DriverConfig cfg;
 
 	// Configure ethernet connection here
-	cfg.set_modem_id(goby::util::as<uint32>(argv[2]));	// CL: user input id
+	cfg.set_modem_id(goby::util::as<uint32>(argv[3]));	// CL: user input id
 	cfg.set_connection_type(goby::acomms::protobuf::DriverConfig::CONNECTION_TCP_AS_CLIENT);
 	cfg.set_tcp_server("127.0.0.1"); 										// CL: test purpose ip address
 	cfg.set_reconnect_interval(1);
-	cfg.set_tcp_port(goby::util::as<uint32>(argv[1])); 	// CL: user input port
+	cfg.set_tcp_port(goby::util::as<uint32>(argv[2])); 	// CL: user input port
 
 	// Connect on completion
 	goby::acomms::connect(&driver->signal_receive, &handle_data_receive);
