7565d0222eb191200ea8447da78932bf7d275ea2
Prachi Nawathe <pnawathe@usc.edu>
2017-04-21 10:53:48 -0700

    more additions to evologics fsm

--- src/acomms/modemdriver/evologics_driver_fsm.h
+++ src/acomms/modemdriver/evologics_driver_fsm.h
@@ -21,3 +21,94 @@
 
 #include "goby/acomms/protobuf/modem_message.pb.h"
 #include "goby/acomms/protobuf/evologics.pb.h"
+
+namespace goby
+{
+    namespace acomms
+    {
+        namespace evologics
+        {
+            struct StateNotify
+            {
+                StateNotify(const std::string& name)
+                : name_(name)
+                {
+                    glog.is(goby::common::logger::DEBUG1) && glog << group("evologicsdriver") << name_ << std::endl;
+                }
+                ~StateNotify()
+                {
+                    glog.is(goby::common::logger::DEBUG1) && glog << group("evologicsdriver") << "~" << name_ << std::endl;
+                }
+            private:
+                std::string name_;
+            }
+            //events
+            struct EvReset : boost::statechart::event< EvReset > {};
+            struct EvATO : boost::statechart::event< EvATO > {};
+            //states
+            struct Active;
+
+            struct Command;
+            struct Configure;
+            struct Ready;
+
+            struct Online;
+            struct Listen;
+            struct TransmitData;
+
+            struct EvologicsFSM : boost::statechart::state_machine<EvologicsFSM, Active>
+            {
+              public:
+              EvologicsFSM(const protobuf::DriverConfig& driver_cfg)
+              {
+                  ++count_;
+                  glog_ir_group_ = "evologicsdriver::" + goby::util::as<std::string>(count_);
+              }
+                //adds message to back of data_out_stream
+                void buffer_data_out(const goby::acomms::protobuf::ModemTransmission& msg);
+
+                // received messages to be passed up out of the ModemDriver
+                boost::circular_buffer<protobuf::ModemTransmission>& received() {return received_;};
+
+                // data that should (eventually) be sent out across the Evologics connection (not sure if necessary)
+                boost::circular_buffer<protobuf::ModemTransmission>& data_out() {return data_out_;}
+
+                const protobuf::DriverConfig& driver_cfg() const {return driver_cfg_;}
+
+                const std::string& glog_ir_group() const { return glog_ir_group_; }
+
+
+              private:
+                boost::circular_buffer<protobuf::ModemTransmission> received_;
+                const protobuf::DriverConfig& driver_cfg_;
+
+                //do we need a data buffer capacity here?
+                boost::circular_buffer<protobuf::ModemTransmission> data_out_;
+
+                std::string glog_ir_group_;
+
+                static int count_;
+            };
+
+            struct Active: boost::statechart::simple_state< Active, EvologicsFSM,
+                 Online >, StateNotify
+            {
+
+                Active() : StateNotify("Active") { }
+                    ~Active() { }
+
+                    typedef boost::mpl::list<
+                        boost::statechart::transition< EvReset, Active >
+                        > reactions;
+            };
+
+            struct Command: boost::statechart::simple_state<Command, Active, Configure>,
+                StateNotify
+            {
+              public:
+
+              private:
+            }
+        }
+    }
+}
